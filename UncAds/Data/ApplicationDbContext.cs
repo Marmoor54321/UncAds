8a3e991be29f6090cc5ec96fa26e7f03ad1e308f
Marmur <89205@student.pb.edu.pl>
2025-10-23 17:32:18 +0200

    Add project files.

---
+++ UncAds/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using UncAds.Models;
+
+namespace UncAds.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Ad> Ads { get; set; }
+    }
+}
---
+++ UncAds/Models/Ad.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UncAds.Models
+{
+    public class Ad
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [Display(Name = "Title")]
+        public string Title { get; set; }
+
+        [Required]
+        [Display(Name = "Description")]
+        [DataType(DataType.MultilineText)]
+        public string Description { get; set; }
+
+        [Display(Name = "Date")]
+        public DateTime Date { get; set; } = DateTime.Now;
+
+        //[Display(Name = "Author")]
+        //public string AuthorId { get; set; }
+
+        //[ForeignKey("AuthorId")]
+        //public virtual User Author { get; set; }
+    }
+}
